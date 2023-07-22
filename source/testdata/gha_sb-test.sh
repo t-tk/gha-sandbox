c953427e8647758e522702af65b4d3942db787c3
TANAKA Takuji <ttk@t-lab.opal.ne.jp>
2023-07-22 11:54:09 +0900

    gha_sb-test.sh: ignore cr for Mingw

--- source/testdata/gha_sb-test.sh
+++ source/testdata/gha_sb-test.sh
@@ -5,9 +5,16 @@ exeext=${exeext:-}
 srcdir=${srcdir:-.}
 _gha_sb=$bindir/gha_sb$exeext
 
+DIFF=diff
+if [ $exeext = ".exe" ]; then
+$DIFF --strip-trailing-cr $0 $0 \
+  && DIFF="diff --strip-trailing-cr" || echo "failed to set --strip-trailing-cr for diff"
+fi
+
 rm -f out.txt
 
 $_gha_sb > out.txt
-diff $srcdir/testdata/gha_sb.txt out.txt || exit 1
+md5sum $srcdir/testdata/gha_sb.txt out.txt
+$DIFF $srcdir/testdata/gha_sb.txt out.txt || exit 1
 
 exit 0
