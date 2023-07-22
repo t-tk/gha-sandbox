bdb0a09d5d0494cf6f79d7771d1482785952e2bc
TANAKA Takuji <ttk@t-lab.opal.ne.jp>
2023-07-22 16:12:29 +0900

    gha_sb-test.sh: remove --strip-trailing-cr from diff

--- source/testdata/gha_sb-test.sh
+++ source/testdata/gha_sb-test.sh
@@ -5,18 +5,12 @@ exeext=${exeext:-}
 srcdir=${srcdir:-.}
 _gha_sb=$bindir/gha_sb$exeext
 
-DIFF=diff
-if [ $exeext = ".exe" ]; then
-$DIFF --strip-trailing-cr $0 $0 \
-  && DIFF="diff --strip-trailing-cr" || echo "failed to set --strip-trailing-cr for diff"
-fi
-
 rm -f ofile.txt out.txt
 
 $_gha_sb --version
 $_gha_sb ofile.txt
 $_gha_sb > out.txt
 md5sum $srcdir/testdata/gha_sb.txt ofile.txt out.txt
-$DIFF $srcdir/testdata/gha_sb.txt out.txt || exit 1
+diff $srcdir/testdata/gha_sb.txt out.txt || exit 1
 
 exit 0
