5f0e9128ca7869b9f35169d4666b16a770fe5e06
TANAKA Takuji <ttk@t-lab.opal.ne.jp>
2023-07-22 11:21:28 +0900

    gha_sb.c: set stdout binary mode (WIN32)

--- source/gha_sb.c
+++ source/gha_sb.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#ifdef WIN32
+#include <fcntl.h>
+#endif
 
 int main() {
+
+#ifdef WIN32
+  _setmode (fileno(stdout), _O_BINARY);
+#endif
+
   printf("this is test.\n");
 
   return 0;
